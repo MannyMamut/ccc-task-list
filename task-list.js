397702570e1510743bfecdf67eaeef35572432b4
Customer Intelligence <customer1@Customers-MacBook-Pro.local>
2018-02-12 20:25:38 -0600

    Efecto de borrado con botón de cerrar

--- task-list.js
+++ task-list.js
@@ -13,19 +13,28 @@ $(document).ready(function(){
 
   var template = '<li class="list-group-item list-group-item-action"></li>';
 
+  $('body').delegate('button.boton-eliminar','click',function( event ){
+    event.preventDefault();
+    if(confirm('¿Está usted completamente seguro?')){
+      var $target = $(this).parent();
+      $target.slideUp(500,function(){
+          $target.remove();
+      });
+    }
+  });
   $formulario_de_lista.on('submit', function( event ){
     event.preventDefault();
     var texto_de_producto = $producto.val().trim();
     var categoria = $categoria.find('option:selected').index();
 
-
     if( texto_de_producto ){
       var clase = categorias[ categoria ];
       var $new_row = $(
         '<li class="list-group-item bg-'+clase.value+' list-group-item-action">' +
-      texto_de_producto +
-      '</li>'
-    );
+        texto_de_producto +
+        '<button class="btn btn-danger btn-sm float-right boton-eliminar"><i class="fas fa-times"></i></button>' +
+        '</li>'
+      );
       $lista_principal.append( $new_row );
 
       $new_row.hide().fadeIn();
